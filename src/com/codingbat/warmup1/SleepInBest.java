ad121ea8598f56304a8e9dc4223cd415993e8749
Marlon Monzon <martianworm17@gmail.com>
2018-10-15 20:23:38 +0800

    Warmup-1 SleepIn. Best Solution.

---
+++ src/com/codingbat/warmup1/SleepInBest.java
@@ -0,0 +1,19 @@
+package com.codingbat.warmup1;
+
+public class SleepInBest {
+
+    public static void main(String[] args) {
+        System.out.println(sleepIn(false, false));
+        System.out.println(sleepIn(true, false));
+        System.out.println(sleepIn(false, true));
+    }
+
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        // Best solution. I must practice to achieve the level of thinking like this!
+        if (!weekday || vacation) {
+            return true;
+        }
+
+        return false;
+    }
+}
