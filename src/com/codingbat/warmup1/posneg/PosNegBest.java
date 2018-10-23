58acbb1ed60ab0662c0ada4691483f470caa9c4b
Marlon Monzon <martianworm17@gmail.com>
2018-10-23 21:08:44 +0800

    Warmup-1 posNeg Best Solution

---
+++ src/com/codingbat/warmup1/posneg/PosNegBest.java
@@ -0,0 +1,18 @@
+package com.codingbat.warmup1.posneg;
+
+public class PosNegBest {
+
+    public static void main(String[] args) {
+        System.out.println(posNeg(1, -1, false));
+        System.out.println(posNeg(-1, 1, false));
+        System.out.println(posNeg(-4, -5, true));
+    }
+
+    private static boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return (a < 0 && b < 0);
+        } else {
+            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+        }
+    }
+}
