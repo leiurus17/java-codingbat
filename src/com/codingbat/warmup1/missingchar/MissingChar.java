3b3111e876be2e7f2cb11fc504b8c6536754659a
Marlon Monzon <martianworm17@gmail.com>
2018-10-24 21:16:35 +0800

    Warmup-1 missingChar

--- src/com/codingbat/warmup1/missingchar/MissingChar.java
+++ src/com/codingbat/warmup1/missingchar/MissingChar.java
@@ -2,5 +2,24 @@ package com.codingbat.warmup1.missingchar;
 
 public class MissingChar {
 
+    public static void main(String[] args) {
+        System.out.println(missingChar("kitten", 1));
+        System.out.println(missingChar("kitten", 0));
+        System.out.println(missingChar("kitten", 4));
+    }
+
+    private static String missingChar(String str, int n) {
+        char[] stringArray = str.toCharArray();
+        String finalString = "";
+        for (int x = 0; x < stringArray.length; x++) {
+            if (x == n) {
+                continue;
+            }
+
+            finalString = finalString + stringArray[x];
+        }
+
+        return finalString;
+    }
 
 }
