2cc96292af53cd28657d524034503f2d6c9346db
Marlon Monzon <martianworm17@gmail.com>
2018-10-27 16:51:37 +0800

    Warmup-1 backAround

--- src/com/codingbat/warmup1/backaround/BackAround.java
+++ src/com/codingbat/warmup1/backaround/BackAround.java
@@ -3,6 +3,14 @@ package com.codingbat.warmup1.backaround;
 public class BackAround {
 
     public static void main(String[] args) {
-        // TODO
+        System.out.println(backAround("cat"));
+        System.out.println(backAround("Hello"));
+        System.out.println(backAround("a"));
+    }
+
+    private static String backAround(String str) {
+        // Yehey! I also got the best solution
+        String last = str.substring(str.length()-1);
+        return last + str + last;
     }
 }
