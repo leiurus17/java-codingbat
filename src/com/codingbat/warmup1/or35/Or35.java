b8bb0cf413ebb5c545b58324d99ed6dc8544399f
Marlon Monzon <martianworm17@gmail.com>
2018-10-28 09:21:56 +0800

    or35 savepoint

---
+++ src/com/codingbat/warmup1/or35/Or35.java
@@ -0,0 +1,14 @@
+package com.codingbat.warmup1.or35;
+
+public class Or35 {
+
+    public static void main(String[] args) {
+        System.out.println(or35(3));
+        System.out.println(or35(10));
+        System.out.println(or35(8));
+    }
+
+    private static boolean or35(int n) {
+        return false;
+    }
+}
