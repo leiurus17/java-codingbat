0b5715a4e518ce29f343a025d069d8591703d3ec
Marlon Monzon <martianworm17@gmail.com>
2018-11-11 08:29:13 +0800

    Warmup-1 startHi solved

--- src/com/codingbat/warmup1/starthi/StartHi.java
+++ src/com/codingbat/warmup1/starthi/StartHi.java
@@ -1,8 +1,18 @@
 package com.codingbat.warmup1.starthi;
 
 public class StartHi {
-    // TODO Implementation
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println(startHi("hi there"));
+        System.out.println(startHi("hi"));
+        System.out.println(startHi("hello hi"));
+    }
+
+    private static boolean startHi(String str) {
+        if (str.length() < 2) {
+            return false;
+        }
+
+        return str.substring(0,2).equalsIgnoreCase("hi");
     }
 }
