f30c28cea6fd207389d7a6b1339764bd40ecd31d
Marlon Monzon <martianworm17@gmail.com>
2018-11-11 08:42:10 +0800

    Warmup-1 icyHot

---
+++ src/com/codingbat/warmup1/icyhot/IcyHot.java
@@ -0,0 +1,18 @@
+package com.codingbat.warmup1.icyhot;
+
+public class IcyHot {
+
+    public static void main(String[] args) {
+        System.out.println(icyHot(120, -1));
+        System.out.println(icyHot(-1, 120));
+        System.out.println(icyHot(2, 120));
+    }
+
+    private static boolean icyHot(int temp1, int temp2) {
+        if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)) {
+            return true;
+        }
+
+        return false;
+    }
+}
