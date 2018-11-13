f72b5fd007b2c2c1a94f7cb211d1ee79cf74b8ea
Marlon Monzon <martianworm17@gmail.com>
2018-11-13 18:37:56 +0800

    Warmup-1 startOz
    This is the best solution. I must again remind myself to think that I can manipulate variables to solve a problem.

---
+++ src/com/codingbat/warmup1/startoz/StartOzBest.java
@@ -0,0 +1,24 @@
+package com.codingbat.warmup1.startoz;
+
+public class StartOzBest {
+
+    public static void main(String[] args) {
+        System.out.println(startOz("ozymandias"));
+        System.out.println(startOz("bzoo"));
+        System.out.println(startOz("oxx"));
+    }
+
+    private static String startOz(String str) {
+        String result = "";
+
+        if (str.length() >= 1 && str.charAt(0)=='o') {
+            result = result + str.charAt(0);
+        }
+
+        if (str.length() >= 2 && str.charAt(1)=='z') {
+            result = result + str.charAt(1);
+        }
+
+        return result;
+    }
+}
