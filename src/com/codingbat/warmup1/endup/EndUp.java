be5065c0f4ffe206f640664539dc8e961c8074e1
Marlon Monzon <martianworm17@gmail.com>
2018-11-15 19:27:40 +0800

    Warmup-1 endUp

--- src/com/codingbat/warmup1/endup/EndUp.java
+++ src/com/codingbat/warmup1/endup/EndUp.java
@@ -1,4 +1,20 @@
 package com.codingbat.warmup1.endup;
 
 public class EndUp {
+
+    public static void main(String[] args) {
+        System.out.println(endUp("Hello"));
+        System.out.println(endUp("hi there"));
+        System.out.println(endUp("hi"));
+    }
+
+    private static String endUp(String str) {
+        int strLength = str.length();
+
+        if (str.length() >= 3) {
+            return str.replace(str.substring(strLength - 3, strLength),(str.substring(strLength - 3, strLength).toUpperCase()));
+        } else {
+            return str.substring(0, strLength).toUpperCase();
+        }
+    }
 }
