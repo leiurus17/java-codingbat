122f2210248b4676f5fe4a4ca72a1218c0ff9351
Marlon Monzon <martianworm17@gmail.com>
2018-11-16 23:22:32 +0800

    Warmup-2 stringTimes - Savepoint

--- src/com/codingbat/warmup2/stringtimes/StringTimes.java
+++ src/com/codingbat/warmup2/stringtimes/StringTimes.java
@@ -3,6 +3,17 @@ package com.codingbat.warmup2.stringtimes;
 public class StringTimes {
 
     public static void main(String[] args) {
-        
+        System.out.println(stringTimes("Hi", 2));
+        System.out.println(stringTimes("Hi", 3));
+        System.out.println(stringTimes("Hi", 1));
+    }
+
+    private static String stringTimes(String str, int n) {
+
+        for (int x = 0; x < n; x++) {
+            str = str + str;
+        }
+
+        return str;
     }
 }
