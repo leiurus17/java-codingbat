3e2434baf52d412b1fd83d9fce5a9b50b3db728a
Marlon Monzon <martianworm17@gmail.com>
2018-11-17 10:26:57 +0800

    Warmup-2 countXX

--- src/com/codingbat/warmup2/countxx/CountXX.java
+++ src/com/codingbat/warmup2/countxx/CountXX.java
@@ -1,4 +1,23 @@
 package com.codingbat.warmup2.countxx;
 
 public class CountXX {
+
+    public static void main(String[] args) {
+        System.out.println(countXX("abcxx"));
+        System.out.println(countXX("xxx"));
+        System.out.println(countXX("xxxx"));
+    }
+
+    private static int countXX(String str) {
+        int count = 0;
+
+        for (int x = 0; x < str.length()-1; x++) {
+
+            if (str.substring(x, x+2).equalsIgnoreCase("xx")) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
