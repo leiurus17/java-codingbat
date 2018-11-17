3364aead89a5bdc7a0553f124b9106c6a96b4239
Marlon Monzon <martianworm17@gmail.com>
2018-11-17 15:51:50 +0800

    Warmup-2 stringSplosion

--- src/com/codingbat/warmup2/stringsplosion/StringSplosion.java
+++ src/com/codingbat/warmup2/stringsplosion/StringSplosion.java
@@ -1,4 +1,20 @@
 package com.codingbat.warmup2.stringsplosion;
 
 public class StringSplosion {
+
+    public static void main(String[] args) {
+        System.out.println(stringSplosion("Code"));
+        System.out.println(stringSplosion("abc"));
+        System.out.println(stringSplosion("ab"));
+    }
+
+    private static String stringSplosion(String str) {
+        String explodedString = "";
+
+        for (int x = 0; x < str.length(); x++) {
+            explodedString = explodedString + str.substring(0, x+1);
+        }
+
+        return explodedString;
+    }
 }
