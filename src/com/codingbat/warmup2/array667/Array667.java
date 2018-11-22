efe9d15698f7f7391581ac82d3b7794b50eecbf6
Marlon Monzon <martianworm17@gmail.com>
2018-11-23 06:42:20 +0800

    Warmup-2 array667 prepare source

---
+++ src/com/codingbat/warmup2/array667/Array667.java
@@ -0,0 +1,12 @@
+package com.codingbat.warmup2.array667;
+
+public class Array667 {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int array667(int[] nums) {
+        return 0;
+    }
+}
