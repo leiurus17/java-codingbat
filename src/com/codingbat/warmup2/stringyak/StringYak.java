f4cc3eff033c2415b2cea8b74bbf66ba7df7717a
Marlon Monzon <martianworm17@gmail.com>
2018-11-22 21:51:12 +0800

    Warmup-2 stringYak

--- src/com/codingbat/warmup2/stringyak/StringYak.java
+++ src/com/codingbat/warmup2/stringyak/StringYak.java
@@ -4,17 +4,27 @@ public class StringYak {
 
     public static void main(String[] args) {
         System.out.println(stringYak("yakpak"));
-        System.out.println(stringYak("pakyak"));
-        System.out.println(stringYak("yak123ya"));
+        //System.out.println(stringYak("pakyak"));
+        //System.out.println(stringYak("yak123ya"));
     }
 
     private static String stringYak(String str) {
         String yak = "yak";
+        String newString = "";
+        int x = 0;
 
-        for (int x = 0; x < str.length() - 2; x++) {
-            // TODO Implementation
+        while(x < str.length()-2) {
+            // TODO Fix this
+            System.out.println(str.substring(x, x+3));
+
+            if (str.substring(x, x+3).equalsIgnoreCase(yak)) {
+                x += 3;
+            } else {
+                newString += str.substring(x,x+1);
+                x++;
+            }
         }
 
-        return "";
+        return newString;
     }
 }
