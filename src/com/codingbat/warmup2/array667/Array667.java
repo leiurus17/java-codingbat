53e992e382923260dc1fef393d4b2af9e8421ec1
Marlon Monzon <martianworm17@gmail.com>
2018-11-26 21:11:50 +0800

    Warmup-2 array667

--- src/com/codingbat/warmup2/array667/Array667.java
+++ src/com/codingbat/warmup2/array667/Array667.java
@@ -3,10 +3,20 @@ package com.codingbat.warmup2.array667;
 public class Array667 {
 
     public static void main(String[] args) {
-
+        System.out.println(array667(new int[]{6,6,2}));
+        System.out.println(array667(new int[]{6,6,2,6}));
+        System.out.println(array667(new int[]{6,7,2,6}));
     }
 
     private static int array667(int[] nums) {
-        return 0;
+        int sixSixSeven = 0;
+
+        for (int x = 0; x < nums.length-1; x++) {
+            if((nums[x] == 6) && (nums[x+1] == 6 || nums[x+1] == 7)) {
+                sixSixSeven++;
+            }
+        }
+
+        return sixSixSeven;
     }
 }
