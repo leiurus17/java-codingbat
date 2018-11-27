113bd648af9681ca56a102b888e8e93b57468883
Marlon Monzon <martianworm17@gmail.com>
2018-11-27 21:45:55 +0800

    Warmup-2 noTriples
    Implement a better solution next time.

---
+++ src/com/codingbat/notriples/NoTriples.java
@@ -0,0 +1,26 @@
+package com.codingbat.notriples;
+
+public class NoTriples {
+
+    public static void main(String[] args) {
+        System.out.println(noTriples(new int[]{1,1,2,2,1}));
+        System.out.println(noTriples(new int[]{1,1,2,2,2,1}));
+        System.out.println(noTriples(new int[]{1,1,1,2,2,2,1}));
+        System.out.println(noTriples(new int[]{1,1,1}));
+    }
+
+    private static boolean noTriples(int[] nums){
+
+        if (nums.length == 3){
+            return !(nums[0] == nums[0+1]) && (nums[0] ==  nums[0+2]);
+        }
+
+        for (int x = 0; x < nums.length-3; x++) {
+            if((nums[x] == nums[x+1]) && (nums[x] ==  nums[x+2])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
