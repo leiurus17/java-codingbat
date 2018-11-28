709fa13b416394df55bd74be2b367cf48a86e9e8
Marlon Monzon <martianworm17@gmail.com>
2018-11-28 22:31:30 +0800

    Warmup-2 has271 - GO UP FIGHTING MAROONS
    Add comment to commemorate this great day for UP Fighting Maroons Men's Basketball Team

--- src/com/codingbat/warmup2/has271/Has271.java
+++ src/com/codingbat/warmup2/has271/Has271.java
@@ -8,6 +8,9 @@ public class Has271 {
         System.out.println(has271(new int[]{2,7,1}));
     }
 
+    // GO UP FIGHTING MAROONS!
+    // BATTLE OF KATIPUNAN!!
+
     private static boolean has271(int... nums) {
         return false;
     }
