f95ba40d1416201fdbfa026d956fa196c74d55ac
Marlon Monzon <martianworm17@gmail.com>
2018-11-29 21:38:40 +0800

    String-1 helloName - Prepare Sources

---
+++ src/com/codingbat/string1/helloname/HelloName.java
@@ -0,0 +1,17 @@
+package com.codingbat.string1.helloname;
+
+public class HelloName {
+
+    public static void main(String[] args) {
+        System.out.println(helloName("Bob"));
+        System.out.println(helloName("Alice"));
+        System.out.println(helloName("X"));
+    }
+
+    // TODO finish first all the Warmup-2 problems
+    // Before doing this one even if it's easy
+
+    private static String helloName(String name) {
+        return "";
+    }
+}
