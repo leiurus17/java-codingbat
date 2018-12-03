71a409292ba0ba47e871b02f4c823de70ac5ffd4
Marlon Monzon <martianworm17@gmail.com>
2018-12-03 21:18:31 +0800

    Warmup-2 has271
    Done. :)

--- src/com/codingbat/warmup2/has271/Has271.java
+++ src/com/codingbat/warmup2/has271/Has271.java
@@ -6,12 +6,22 @@ public class Has271 {
         System.out.println(has271(new int[]{1,2,7,1}));
         System.out.println(has271(new int[]{1,2,8,1}));
         System.out.println(has271(new int[]{2,7,1}));
+        System.out.println(has271(new int[]{2,7,4}));
+        System.out.println(has271(new int[]{2,7,-1}));
+        System.out.println(has271(new int[]{4,5,3,8,0}));
     }
 
     // GO UP FIGHTING MAROONS!
     // BATTLE OF KATIPUNAN!!
 
     private static boolean has271(int... nums) {
+        for (int x = 0; x < nums.length -1; x++) {
+
+            if ((nums[x] + 5 == nums[x+1]) && (Math.abs(Math.abs(nums[x] - 1) - nums[x+2]) <= 2)) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
