3ca34ff030cab40916d08f7c906724d7c5c37f4f
Marlon Monzon <martianworm17@gmail.com>
2018-12-05 06:56:30 +0800

    String-1 makeOutWord

---
+++ src/com/codingbat/string1/makeoutword/MakeOutWord.java
@@ -0,0 +1,12 @@
+package com.codingbat.string1.makeoutword;
+
+public class MakeOutWord {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static String makeOutWord(String out, String word) {
+        return "";
+    }
+}
