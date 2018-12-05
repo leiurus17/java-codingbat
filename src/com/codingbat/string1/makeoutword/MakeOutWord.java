11f1380a8f3a1a4af2b68f07b5765ba2ffb43661
Marlon Monzon <martianworm17@gmail.com>
2018-12-05 23:20:37 +0800

    String-1 makeOutWord

--- src/com/codingbat/string1/makeoutword/MakeOutWord.java
+++ src/com/codingbat/string1/makeoutword/MakeOutWord.java
@@ -3,10 +3,12 @@ package com.codingbat.string1.makeoutword;
 public class MakeOutWord {
 
     public static void main(String[] args) {
-
+        System.out.println(makeOutWord("<<>>", "Yay"));
+        System.out.println(makeOutWord("<<>>", "WooHoo"));
+        System.out.println(makeOutWord("[[]]", "word"));
     }
 
     private static String makeOutWord(String out, String word) {
-        return "";
+        return out.substring(0,2).concat(word).concat(out.substring(2));
     }
 }
