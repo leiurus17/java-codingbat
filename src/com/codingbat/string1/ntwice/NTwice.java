de1b19b597e9581d8d87d9e3d47322000aa2966f
Marlon Monzon <martianworm17@gmail.com>
2018-12-07 06:52:50 +0800

    String-1 nTwice

---
+++ src/com/codingbat/string1/ntwice/NTwice.java
@@ -0,0 +1,12 @@
+package com.codingbat.string1.ntwice;
+
+public class NTwice {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static String nTwice(String str, int n) {
+        return "";
+    }
+}
