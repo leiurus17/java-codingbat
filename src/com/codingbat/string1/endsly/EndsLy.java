50ad737260b1039ba0cfc2874d49d49340540abb
Marlon Monzon <martianworm17@gmail.com>
2018-12-07 06:50:25 +0800

    String-1 endsLy

---
+++ src/com/codingbat/string1/endsly/EndsLy.java
@@ -0,0 +1,12 @@
+package com.codingbat.string1.endsly;
+
+public class EndsLy {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static boolean endsLy(String str) {
+        return false;
+    }
+}
