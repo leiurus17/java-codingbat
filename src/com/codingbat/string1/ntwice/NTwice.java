655e2d6cee5a9d5c1cd6e82e72af4cf6fa56e102
Marlon Monzon <martianworm17@gmail.com>
2018-12-08 07:12:23 +0800

    String-1 nTwice

--- src/com/codingbat/string1/ntwice/NTwice.java
+++ src/com/codingbat/string1/ntwice/NTwice.java
@@ -3,10 +3,12 @@ package com.codingbat.string1.ntwice;
 public class NTwice {
 
     public static void main(String[] args) {
-
+        System.out.println(nTwice("Hello", 2));
+        System.out.println(nTwice("Chocolate", 3));
+        System.out.println(nTwice("Chocolate", 1));
     }
 
     private static String nTwice(String str, int n) {
-        return "";
+        return str.substring(0,n).concat(str.substring(str.length()-n));
     }
 }
