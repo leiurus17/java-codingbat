986ab75f6f5efa5e6d2a3fd66e3c4694eed5f872
Marlon Monzon <martianworm17@gmail.com>
2018-12-08 08:57:06 +0800

    String-1 twoChar - Still wrong implementation

--- src/com/codingbat/string1/twochar/TwoChar.java
+++ src/com/codingbat/string1/twochar/TwoChar.java
@@ -6,10 +6,12 @@ public class TwoChar {
         System.out.println(twoChar("java", 0));
         System.out.println(twoChar("java", 2));
         System.out.println(twoChar("java", 3));
+        System.out.println(twoChar("Hello", 1));
+        System.out.println(twoChar("Hello", 3));
     }
 
     // FIXME there is a failing test
     private static String twoChar(String str, int index) {
-        return index != 2 ? str.substring(0,2) : str.substring(index);
+        return index != 2 && str.length() - index < 2 || index < 0 ? str.substring(0,2) : str.substring(index,index+2);
     }
 }
