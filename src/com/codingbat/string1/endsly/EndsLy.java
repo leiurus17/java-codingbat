c7191978be58909abad47f56f1928367c36289e4
Marlon Monzon <martianworm17@gmail.com>
2018-12-09 10:05:55 +0800

    String-1 endsLy

--- src/com/codingbat/string1/endsly/EndsLy.java
+++ src/com/codingbat/string1/endsly/EndsLy.java
@@ -3,10 +3,12 @@ package com.codingbat.string1.endsly;
 public class EndsLy {
 
     public static void main(String[] args) {
-
+        System.out.println(endsLy("oddly"));
+        System.out.println(endsLy("y"));
+        System.out.println(endsLy("oddy"));
     }
 
     private static boolean endsLy(String str) {
-        return false;
+        return str.length() < 2 ? false : str.substring(str.length()-2).equalsIgnoreCase("ly");
     }
 }
