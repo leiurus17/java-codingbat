3d7dc75c1abb792b76a21ded67dd2e523a4ab6f5
Marlon Monzon <martianworm17@gmail.com>
2018-12-10 07:04:35 +0800

    String-1 without2

---
+++ src/com/codingbat/string1/without2/Without2.java
@@ -0,0 +1,15 @@
+package com.codingbat.string1.without2;
+
+public class Without2 {
+
+    public static void main(String[] args) {
+        System.out.println(without2("HelloHe"));
+        System.out.println(without2("HelloHi"));
+        System.out.println(without2("Hi"));
+    }
+
+    //TODO implement
+    private static String without2(String str) {
+        return "";
+    }
+}
