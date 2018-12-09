8d1ab690dfdf42a148c18dc96a0464aef20335d3
Marlon Monzon <martianworm17@gmail.com>
2018-12-09 21:33:44 +0800

    String-1 seeColor

---
+++ src/com/codingbat/string1/seecolor/SeeColor.java
@@ -0,0 +1,25 @@
+package com.codingbat.string1.seecolor;
+
+public class SeeColor {
+
+    public static void main(String[] args) {
+        System.out.println(seeColor("redxx"));
+        System.out.println(seeColor("xxred"));
+        System.out.println(seeColor("blueTimes"));
+    }
+
+    //TODO I can still improve this
+    private static String seeColor(String str) {
+        if (str.length() < 3) {
+            return "";
+        } else if (str.substring(0,3).equalsIgnoreCase("red")) {
+            return "red";
+        } else if (!str.substring(0,3).equalsIgnoreCase("red") && str.length()==3) {
+          return "";
+        } else if (str.substring(0,4).equalsIgnoreCase("blue")) {
+            return "blue";
+        } else {
+            return "";
+        }
+    }
+}
