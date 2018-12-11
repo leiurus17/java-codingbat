3b6bcfd3861917289d54e8de778ea024d4a0349d
Marlon Monzon <martianworm17@gmail.com>
2018-12-11 20:37:20 +0800

    String-1 deFront - Prepare sources

---
+++ src/com/codingbat/string1/defront/DeFront.java
@@ -0,0 +1,14 @@
+package com.codingbat.string1.defront;
+
+public class DeFront {
+
+    public static void main(String[] args) {
+        System.out.println(deFront("Hello"));
+        System.out.println(deFront("java"));
+        System.out.println(deFront("away"));
+    }
+
+    public static String deFront(String str) {
+        return "";
+    }
+}
