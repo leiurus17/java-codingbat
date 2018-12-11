bcc5ac1d15d7e0e2330d1aad50e72ea695b80ae9
Marlon Monzon <martianworm17@gmail.com>
2018-12-12 07:03:49 +0800

    String-1 startWord - Prepare sources

---
+++ src/com/codingbat/string1/startword/StartWord.java
@@ -0,0 +1,14 @@
+package com.codingbat.string1.startword;
+
+public class StartWord {
+
+    public static void main(String[] args) {
+        System.out.println(startWord("hippo", "hi"));
+        System.out.println(startWord("hippo", "xip"));
+        System.out.println(startWord("hippo", "i"));
+    }
+
+    private static String startWord(String str, String word) {
+        return "";
+    }
+}
