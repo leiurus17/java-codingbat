e4e8786b16ca080367ae14a94bed2c1524c9de31
Marlon Monzon <martianworm17@gmail.com>
2018-12-13 06:57:19 +0800

    String-1 withoutX

---
+++ src/com/codingbat/string1/withoutx/WithoutX.java
@@ -0,0 +1,14 @@
+package com.codingbat.string1.withoutx;
+
+public class WithoutX {
+
+    public static void main(String[] args) {
+        System.out.println(withoutX("xHix"));
+        System.out.println(withoutX("xHi"));
+        System.out.println(withoutX("Hxix"));
+    }
+
+    private static String withoutX(String str) {
+        return "";
+    }
+}
