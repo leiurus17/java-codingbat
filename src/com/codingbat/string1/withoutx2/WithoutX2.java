1135247af07bc381c3bfc75353031fde22c1a346
Marlon Monzon <martianworm17@gmail.com>
2018-12-14 00:02:54 +0800

    String-1 withoutX2

---
+++ src/com/codingbat/string1/withoutx2/WithoutX2.java
@@ -0,0 +1,14 @@
+package com.codingbat.string1.withoutx2;
+
+public class WithoutX2 {
+
+    public static void main(String[] args) {
+        System.out.println(withoutX2("xHi"));
+        System.out.println(withoutX2("Hxi"));
+        System.out.println(withoutX2("Hi"));
+    }
+
+    private static String withoutX2(String str) {
+        return "";
+    }
+}
