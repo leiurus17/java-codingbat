0a00cc8ff7a56da189ab34c1757612d02fcc0e44
Marlon Monzon <martianworm17@gmail.com>
2018-12-17 22:16:39 +0800

    Array-1 firstLast6

--- src/com/codingbat/array1/firstlast6/FirstLast6.java
+++ src/com/codingbat/array1/firstlast6/FirstLast6.java
@@ -1,4 +1,14 @@
 package com.codingbat.array1.firstlast6;
 
 public class FirstLast6 {
+
+    public static void main(String[] args) {
+        System.out.println(firstLast6(new int[] {1, 2, 6}));
+        System.out.println(firstLast6(new int[] {6, 1, 2, 3}));
+        System.out.println(firstLast6(new int[] {13, 6, 1, 2, 3}));
+    }
+
+    private static boolean firstLast6(int[] nums) {
+        return nums[0] == 6 || nums[nums.length-1] == 6;
+    }
 }
