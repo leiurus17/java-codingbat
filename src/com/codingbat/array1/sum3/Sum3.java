8b2833d5936b2294e727d5113958622e08fcb459
Marlon Monzon <martianworm17@gmail.com>
2018-12-18 21:20:43 +0800

    Array-1 sum3

---
+++ src/com/codingbat/array1/sum3/Sum3.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.sum3;
+
+public class Sum3 {
+
+    public static void main(String[] args) {
+        System.out.println(sum3(new int[] {1, 2, 3}));
+        System.out.println(sum3(new int[] {5, 11, 2}));
+        System.out.println(sum3(new int[] {7, 0, 0}));
+    }
+
+    // TODO implement
+    private static int sum3(int[] nums) {
+
+    }
+}
