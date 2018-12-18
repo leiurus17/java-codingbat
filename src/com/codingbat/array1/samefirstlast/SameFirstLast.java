f8dee8885dcc0d0da90e65798e11b89b3f43a504
Marlon Monzon <martianworm17@gmail.com>
2018-12-18 20:47:17 +0800

    Array-1 sameFirstLast

--- src/com/codingbat/array1/samefirstlast/SameFirstLast.java
+++ src/com/codingbat/array1/samefirstlast/SameFirstLast.java
@@ -1,4 +1,14 @@
 package com.codingbat.array1.samefirstlast;
 
 public class SameFirstLast {
+
+    public static void main(String[] args) {
+        System.out.println(sameFirstLast(new int[] {1, 2, 3}));
+        System.out.println(sameFirstLast(new int[] {1, 2, 3, 1}));
+        System.out.println(sameFirstLast(new int[] {1, 2, 1}));
+    }
+
+    public static boolean sameFirstLast(int[] nums) {
+        return nums.length > 0 && nums[0] == nums[nums.length-1];
+    }
 }
