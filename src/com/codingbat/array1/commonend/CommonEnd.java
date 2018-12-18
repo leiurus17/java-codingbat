c10580573896a87a0c4cff5190a68dc72c8dee4b
Marlon Monzon <martianworm17@gmail.com>
2018-12-18 20:58:41 +0800

    Array-1 commonEnd

---
+++ src/com/codingbat/array1/commonend/CommonEnd.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.commonend;
+
+public class CommonEnd {
+
+    public static void main(String[] args) {
+        System.out.println(commonEnd(new int[] {1, 2, 3}, new int[] {7, 3}));
+        System.out.println(commonEnd(new int[] {1, 2, 3}, new int[] {7, 3, 2}));
+        System.out.println(commonEnd(new int[] {1, 2, 3}, new int[] {1, 3}));
+    }
+
+    // TODO implement
+    public static boolean commonEnd(int[] a, int[] b) {
+        return false;
+    }
+}
