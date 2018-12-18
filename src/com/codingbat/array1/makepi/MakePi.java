fc321557f20cc40d6d192b549fc06c667398e8c3
Marlon Monzon <martianworm17@gmail.com>
2018-12-18 20:30:18 +0800

    Array-1 makePi

--- src/com/codingbat/array1/makepi/MakePi.java
+++ src/com/codingbat/array1/makepi/MakePi.java
@@ -1,4 +1,12 @@
 package com.codingbat.array1.makepi;
 
 public class MakePi {
+
+    public static void main(String[] args) {
+        System.out.println(makePi());
+    }
+
+    public static int[] makePi() {
+        return new int[] {3,1,4};
+    }
 }
