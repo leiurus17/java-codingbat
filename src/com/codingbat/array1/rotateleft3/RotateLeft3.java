624141e49f55787e361ed501fa220457e23eb0eb
Marlon Monzon <martianworm17@gmail.com>
2018-12-18 21:26:40 +0800

    Array-1 rotateLeft3

---
+++ src/com/codingbat/array1/rotateleft3/RotateLeft3.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.rotateleft3;
+
+public class RotateLeft3 {
+
+    public static void main(String[] args) {
+        System.out.println(rotateLeft3(new int[] {1, 2, 3}));
+        System.out.println(rotateLeft3(new int[] {5, 11, 9}));
+        System.out.println(rotateLeft3(new int[] {0, 0, 7}));
+    }
+
+    // TODO implement
+    private static int[] rotateLeft3(int[] nums) {
+        return new int[] {0};
+    }
+}
