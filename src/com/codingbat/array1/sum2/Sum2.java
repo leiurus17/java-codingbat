ff8ca231b596a7f30cc8e06b0db7a8f305402f86
Marlon Monzon <martianworm17@gmail.com>
2018-12-19 20:12:34 +0800

    Array-1 sum2

---
+++ src/com/codingbat/array1/sum2/Sum2.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.sum2;
+
+public class Sum2 {
+
+    public static void main(String[] args) {
+        System.out.println(sum2(new int[] {1, 2, 3}));
+        System.out.println(sum2(new int[] {1, 1}));
+        System.out.println(sum2(new int[] {1, 1, 1, 1}));
+    }
+
+    // TODO implement
+    private static int sum2(int[] nums) {
+        return 0;
+    }
+}
