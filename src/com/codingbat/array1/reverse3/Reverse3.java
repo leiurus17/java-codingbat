5e74f661844ffc2969382d8f239d991b6e883f26
Marlon Monzon <martianworm17@gmail.com>
2018-12-19 19:53:46 +0800

    Array-1 reverse3

---
+++ src/com/codingbat/array1/reverse3/Reverse3.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.reverse3;
+
+public class Reverse3 {
+
+    public static void main(String[] args) {
+        System.out.println(reverse3(new int[] {1, 2, 3}));
+        System.out.println(reverse3(new int[] {5, 11, 9}));
+        System.out.println(reverse3(new int[] {7, 0, 0}));
+    }
+
+    // TODO implement
+    private static int[] reverse3(int[] nums) {
+        return new int[] {0};
+    }
+}
