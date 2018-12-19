5a72a9fea458458f7514eda8d0144ba866b5e9fe
Marlon Monzon <martianworm17@gmail.com>
2018-12-19 19:55:44 +0800

    Array-1 maxEnd3

---
+++ src/com/codingbat/array1/maxend3/MaxEnd3.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.maxend3;
+
+public class MaxEnd3 {
+
+    public static void main(String[] args) {
+        System.out.println(maxEnd3(new int[] {1, 2, 3}));
+        System.out.println(maxEnd3(new int[] {11, 5, 9}));
+        System.out.println(maxEnd3(new int[] {2, 11, 3}));
+    }
+
+    // TODO implement
+    private static int[] maxEnd3(int[] nums) {
+        return new int[] {0};
+    }
+}
