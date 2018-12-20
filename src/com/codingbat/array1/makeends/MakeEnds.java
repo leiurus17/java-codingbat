f9ed56d14bc59b1e3ae97f5ae6788fd9e5fefc48
Marlon Monzon <martianworm17@gmail.com>
2018-12-20 18:33:17 +0800

    Array-1 makeEnds

---
+++ src/com/codingbat/array1/makeends/MakeEnds.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.makeends;
+
+public class MakeEnds {
+
+    public static void main(String[] args) {
+        System.out.println(makeEnds(new int[] {1, 2, 3}));
+        System.out.println(makeEnds(new int[] {1, 2, 3, 4}));
+        System.out.println(makeEnds(new int[] {7, 4, 6, 2}));
+    }
+
+    // TODO implement
+    private static int[] makeEnds(int[] nums) {
+        return new int[] {0};
+    }
+}
