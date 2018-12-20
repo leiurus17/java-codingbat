60be857d60a6c55285f822cc15dffa2fda848688
Marlon Monzon <martianworm17@gmail.com>
2018-12-20 18:57:42 +0800

    Array-1 has23

---
+++ src/com/codingbat/array1/has23/Has23.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.has23;
+
+public class Has23 {
+
+    public static void main(String[] args) {
+        System.out.println(has23(new int[] {2, 5}));
+        System.out.println(has23(new int[] {4, 3}));
+        System.out.println(has23(new int[] {4, 5}));
+    }
+
+    // TODO implement
+    private static boolean has23(int[] nums) {
+        return false;
+    }
+}
