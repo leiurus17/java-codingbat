7b49e2ea1242131b2fbb5ad017a1f68fea5ba5d4
Marlon Monzon <martianworm17@gmail.com>
2018-12-20 19:05:58 +0800

    Array-1 no23

---
+++ src/com/codingbat/array1/no23/No23.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.no23;
+
+public class No23 {
+
+    public static void main(String[] args) {
+        System.out.println(no23(new int[] {4, 5}));
+        System.out.println(no23(new int[] {4, 2}));
+        System.out.println(no23(new int[] {3, 5}));
+    }
+
+    // TODO implement
+    private static boolean no23(int[] nums) {
+        return false;
+    }
+}
