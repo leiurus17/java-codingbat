d35c499982a5f22e372e6a4712854092173c5737
Marlon Monzon <martianworm17@gmail.com>
2018-12-20 18:29:32 +0800

    Array-1 middleWay

---
+++ src/com/codingbat/array1/middleway/MiddleWay.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.middleway;
+
+public class MiddleWay {
+
+    public static void main(String[] args) {
+        System.out.println(middleWay(new int[] {1, 2, 3}, new int[] {4, 5, 6}));
+        System.out.println(middleWay(new int[] {7, 7, 7}, new int[] {3, 8, 0}));
+        System.out.println(middleWay(new int[] {5, 2, 9}, new int[] {1, 4, 5}));
+    }
+
+    // TODO implement
+    private static int[] middleWay(int[] a, int[] b) {
+        return new int[] {0};
+    }
+}
