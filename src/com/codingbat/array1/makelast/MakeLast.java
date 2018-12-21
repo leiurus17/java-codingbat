a0e5667d2bc4ccff996cc4ac25b1f7df1a5e988b
Marlon Monzon <martianworm17@gmail.com>
2018-12-21 22:57:11 +0800

    Array-1 makeLast

---
+++ src/com/codingbat/array1/makelast/MakeLast.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.makelast;
+
+public class MakeLast {
+
+    public static void main(String[] args) {
+        System.out.println(makeLast(new int[] {4, 5, 6}));
+        System.out.println(makeLast(new int[] {1, 2}));
+        System.out.println(makeLast(new int[] {3}));
+    }
+
+    // TODO implement
+    private static int[] makeLast(int[] nums) {
+        return new int[] {0};
+    }
+}
