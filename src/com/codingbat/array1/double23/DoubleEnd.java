e3ede14933603f397f73a7a678f34afef8ac18b0
Marlon Monzon <martianworm17@gmail.com>
2018-12-22 11:37:20 +0800

    Array-1 double23

---
+++ src/com/codingbat/array1/double23/DoubleEnd.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.double23;
+
+public class DoubleEnd {
+
+    public static void main(String[] args) {
+        System.out.println(double23(new int[] {2, 2}));
+        System.out.println(double23(new int[] {3, 3}));
+        System.out.println(double23(new int[] {2, 3}));
+    }
+
+    // TODO implement
+    private static boolean double23(int[] nums) {
+        return false;
+    }
+}
