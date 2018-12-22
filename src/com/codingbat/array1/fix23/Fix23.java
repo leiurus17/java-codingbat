5e52eb1397e1ae26db2ad4434ead674c6616fbaf
Marlon Monzon <martianworm17@gmail.com>
2018-12-22 11:41:59 +0800

    Array-1 fix23

---
+++ src/com/codingbat/array1/fix23/Fix23.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.fix23;
+
+public class Fix23 {
+
+    public static void main(String[] args) {
+        System.out.println(fix23(new int[] {1 ,2 ,3}));
+        System.out.println(fix23(new int[] {2 ,3 ,5}));
+        System.out.println(fix23(new int[] {1 ,2 ,1}));
+    }
+
+    // TODO implement
+    private static int[] fix23(int[] nums) {
+        return new int[] {0};
+    }
+}
