0f84533c2e219a7b0cd282f2913ee2e478bb1c5e
Marlon Monzon <martianworm17@gmail.com>
2018-12-23 13:45:35 +0800

    Array-1 make2

---
+++ src/com/codingbat/array1/make2/Make2.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.make2;
+
+public class Make2 {
+
+    public static void main(String[] args) {
+        System.out.println(make2(new int[] {4, 5}, new int[] {1, 2, 3}));
+        System.out.println(make2(new int[] {4}, new int[] {1, 2, 3}));
+        System.out.println(make2(new int[] {}, new int[] {1, 2}));
+    }
+
+    // TODO implement
+    private static int[] make2(int[] a, int[] b) {
+        return new int[] {};
+    }
+}
