bb3c126c5caff1d7087b8e2277fb42639f6dee48
Marlon Monzon <martianworm17@gmail.com>
2018-12-23 09:29:13 +0800

    Array-1 swapEnds

---
+++ src/com/codingbat/array1/swapends/SwapEnds.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.swapends;
+
+public class SwapEnds {
+
+    public static void main(String[] args) {
+        System.out.println(swapEnds(new int[] {1, 2, 3 ,4}));
+        System.out.println(swapEnds(new int[] {1, 2, 3}));
+        System.out.println(swapEnds(new int[] {8, 6, 7, 9, 5}));
+    }
+
+    // TODO implement
+    private static int[] swapEnds(int[] nums) {
+        return new int[] {};
+    }
+}
