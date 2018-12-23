e4cf5f61356e4170b01d77d0cd68299348308e00
Marlon Monzon <martianworm17@gmail.com>
2018-12-23 09:23:34 +0800

    Array-1 plusTwo

---
+++ src/com/codingbat/array1/plustwo/plustwo.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.plustwo;
+
+public class plustwo {
+
+    public static void main(String[] args) {
+        System.out.println(plusTwo(new int[] {1, 2}, new int[] {3, 4}));
+        System.out.println(plusTwo(new int[] {4, 4}, new int[] {2, 2}));
+        System.out.println(plusTwo(new int[] {9 ,2}, new int[] {3, 4}));
+    }
+
+    // TODO implement
+    private static int[] plusTwo(int[] a, int[] b) {
+        return new int[] {};
+    }
+}
