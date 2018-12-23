2a51b57c8eab9d6c7626edd99cad2aa6ceda5bf9
Marlon Monzon <martianworm17@gmail.com>
2018-12-23 09:03:10 +0800

    Array-1 start1

---
+++ src/com/codingbat/array1/start1/Start1.java
@@ -0,0 +1,14 @@
+package com.codingbat.array1.start1;
+
+public class Start1 {
+
+    public static void main(String[] args) {
+        System.out.println(start1(new int[] {1, 2, 3}, new int[] {1, 3}));
+        System.out.println(start1(new int[] {7, 2, 3}, new int[] {1}));
+        System.out.println(start1(new int[] {1, 2}, new int[] {}));
+    }
+
+    private static int start1(int[] a, int[] b) {
+        return 0;
+    }
+}
