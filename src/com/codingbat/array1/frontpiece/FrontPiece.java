4044454497b17eb7ed06db82c80908ef50c9aa8c
Marlon Monzon <martianworm17@gmail.com>
2018-12-23 13:39:44 +0800

    Array-1 frontPiece

---
+++ src/com/codingbat/array1/frontpiece/FrontPiece.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.frontpiece;
+
+public class FrontPiece {
+
+    public static void main(String[] args) {
+        System.out.println(frontPiece(new int[] {1, 2, 3}));
+        System.out.println(frontPiece(new int[] {1, 2}));
+        System.out.println(frontPiece(new int[] {1}));
+    }
+
+    // TODO implement
+    private static int[] frontPiece(int[] nums) {
+        return new int[] {};
+    }
+}
