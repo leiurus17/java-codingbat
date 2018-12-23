0fba259c531cc6c0aa5935912c6f66f492e8fdf7
Marlon Monzon <martianworm17@gmail.com>
2018-12-23 13:41:44 +0800

    Array-1 unlucky1

---
+++ src/com/codingbat/array1/unlucky1/Unlucky1.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.unlucky1;
+
+public class Unlucky1 {
+
+    public static void main(String[] args) {
+        System.out.println(unlucky1(new int[] {1, 3, 4, 5}));
+        System.out.println(unlucky1(new int[] {2, 1, 3, 4, 5}));
+        System.out.println(unlucky1(new int[] {1, 1, 1}));
+    }
+
+    // TODO implement
+    private static boolean unlucky1(int[] nums) {
+        return false;
+    }
+}
