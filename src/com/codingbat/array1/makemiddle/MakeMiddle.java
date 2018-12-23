256b38c19265c505858c00a9eb0ac57ed3f2263e
Marlon Monzon <martianworm17@gmail.com>
2018-12-23 09:11:12 +0800

    Array-1 makeMiddle

---
+++ src/com/codingbat/array1/makemiddle/MakeMiddle.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.makemiddle;
+
+public class MakeMiddle {
+
+    public static void main(String[] args) {
+        System.out.println(makeMiddle(new int[] {1, 2, 3, 4}));
+        System.out.println(makeMiddle(new int[] { 7, 1, 2, 3, 4, 9}));
+        System.out.println(makeMiddle(new int[] {1, 2}));
+    }
+
+    // TODO implement
+    private static int[] makeMiddle(int[] nums) {
+        return new int[] {};
+    }
+}
