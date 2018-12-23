e25ea3484b614f04c00a8d8328481f26fe2d24a6
Marlon Monzon <martianworm17@gmail.com>
2018-12-23 12:44:53 +0800

    Array-1 maxTriple

---
+++ src/com/codingbat/array1/maxTriple/MaxTriple.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.maxTriple;
+
+public class MaxTriple {
+
+    public static void main(String[] args) {
+        System.out.println(maxTriple(new int[] {1, 2, 3}));
+        System.out.println(maxTriple(new int[] {1, 5, 3}));
+        System.out.println(maxTriple(new int[] {5, 2, 3}));
+    }
+
+    // TODO implement
+    private static int maxTriple(int[] nums) {
+        return 0;
+    }
+}
