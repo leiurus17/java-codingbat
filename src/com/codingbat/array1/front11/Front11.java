746efde662682ec7729e2998dc62ca0d42453fa6
Marlon Monzon <martianworm17@gmail.com>
2018-12-23 13:49:58 +0800

    Array-1 front11

---
+++ src/com/codingbat/array1/front11/Front11.java
@@ -0,0 +1,15 @@
+package com.codingbat.array1.front11;
+
+public class Front11 {
+
+    public static void main(String[] args) {
+        System.out.println(front11(new int[] {1, 2, 3}, new int[] {7, 9, 8}));
+        System.out.println(front11(new int[] {1}, new int[] {2}));
+        System.out.println(front11(new int[] {1, 7}, new int[] {}));
+    }
+
+    // TODO implement
+    private static int[] front11(int[] a, int[] b) {
+
+    }
+}
