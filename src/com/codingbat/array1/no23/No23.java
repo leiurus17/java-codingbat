c63df70a151bc65d2d9b60f0aa9df127d3ded953
Marlon Monzon <martianworm17@gmail.com>
2018-12-25 11:28:45 +0800

    Array-1 no23

--- src/com/codingbat/array1/no23/No23.java
+++ src/com/codingbat/array1/no23/No23.java
@@ -8,8 +8,7 @@ public class No23 {
         System.out.println(no23(new int[] {3, 5}));
     }
 
-    // TODO implement
     private static boolean no23(int[] nums) {
-        return false;
+        return !(nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3);
     }
 }
