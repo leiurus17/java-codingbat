3f85fe398993fb7a3bc37929da75ccabebb1b147
Marlon Monzon <martianworm17@gmail.com>
2018-12-25 11:28:55 +0800

    Array-1 has23

--- src/com/codingbat/array1/has23/Has23.java
+++ src/com/codingbat/array1/has23/Has23.java
@@ -9,6 +9,6 @@ public class Has23 {
     }
 
     private static boolean has23(int[] nums) {
-        return nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3;
+        return !(nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3);
     }
 }
