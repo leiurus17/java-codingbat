96673594e5d50bc3251c1a3271f5d4827cf81997
Marlon Monzon <martianworm17@gmail.com>
2019-04-01 22:23:23 +0800

    Array-1 biggerTwo

--- src/com/codingbat/array1/biggertwo/BiggerTwo.java
+++ src/com/codingbat/array1/biggertwo/BiggerTwo.java
@@ -8,8 +8,12 @@ public class BiggerTwo {
         System.out.println(biggerTwo(new int[] {1, 1}, new int[] {1, 2}));
     }
 
-    // TODO implement
     private static int[] biggerTwo(int[] a, int[] b) {
-        return new int[] {};
+
+        if (a[0] + a[1] >= b[0] + b[1]) {
+            return a;
+        } else {
+            return b;
+        }
     }
 }
